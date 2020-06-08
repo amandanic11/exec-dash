7980fc4ff74e931604758e654a89a448283f3ab7
amandanic11 <65553398+amandanic11@users.noreply.github.com>
2020-06-08 15:43:21 -0400

    User prompted to open file from database

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -1,5 +1,20 @@
 # dashboard_generator.py
 
+import pandas as pd
+import os
+import plotly
+import easygui
+import datetime
+
+data = os.path.join("data")
+filepath = os.path.dirname(data)
+
+#User prompted to select file
+filename = easygui.fileopenbox(msg="Please Select a File", default="data/*")
+
+#TODO Calculate total sales for each item and total sales overall
+#TODO Have the program output at least one chart or graph depicting relevant info
+
 print("-----------------------")
 print("MONTH: March 2018")
 
